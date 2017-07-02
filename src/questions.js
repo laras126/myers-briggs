0a9f178859382a6bcb1034e316c4e5e1fdfd417d
laras126 <laras126@gmail.com>
2017-07-02 16:32:06 -0700

    update questions, add js type

--- src/questions.js
+++ src/questions.js
@@ -18,60 +18,60 @@ export let questions = [
     text: 'I can write a regex easily.',
     type: 'back',
   },
-  // {
-  //   text: 'I know the difference between == and ===.',
-  //   type: 'back',
-  // },
-  // {
-  //   text: 'I know the difference between call() and apply().',
-  //   type: 'back',
-  // },
-  // {
-  //   text: 'I understand how the browser renders a webpage.',
-  //   type: 'back',
-  // },
-  // {
-  //   text: 'I am comfortable recommending server-side caching techniques.',
-  //   type: 'back',
-  // },
-  // {
-  //   text: 'I enjoy designing page layouts.',
-  //   type: 'design',
-  // },
+  {
+    text: 'I know the difference between == and ===.',
+    type: 'js',
+  },
+  {
+    text: 'I know the difference between call() and apply().',
+    type: 'js',
+  },
+  {
+    text: 'I understand how the browser renders a webpage.',
+    type: 'back',
+  },
+  {
+    text: 'I am comfortable recommending server-side caching techniques.',
+    type: 'back',
+  },
+  {
+    text: 'I enjoy designing page layouts.',
+    type: 'design',
+  },
   {
     text: 'I enjoy architecting a stylesheet.',
     type: 'front',
   },
-  // {
-  //   text: 'Alan Turing, Grace Hopper, and Claude Shannon are heroes.',
-  //   type: 'back',
-  // },
+  {
+    text: 'Alan Turing, Grace Hopper, and Claude Shannon are heroes.',
+    type: 'back',
+  },
   {
     text: 'Paula Scher, Dieter Rams, and Stefan Sagmeister are heroes.',
     type: 'design',
   },
-  // {
-  //   text: 'Jeffrey Zeldman, Eric Meyer, and Chris Coyier are heroes.',
-  //   type: 'front',
-  // },
-  // {
-  //   text: 'Bad kerning bothers me.',
-  //   type: 'design',
-  // },
-  // {
-  //   text: 'I can style a satisfying-to-click button.',
-  //   type: 'front',
-  // },
-  // {
-  //   text: 'This bothers me: &lt;IMG SRC=\"\">.',
-  //   type: 'front',
-  // },
-  // {
-  //   text: 'I enjoy debugging CSS.',
-  //   type: 'front',
-  // },
-  // {
-  //   text: 'I know my way around dev tools.',
-  //   type: 'front',
-  // },
+  {
+    text: 'Jeffrey Zeldman, Eric Meyer, and Chris Coyier are heroes.',
+    type: 'front',
+  },
+  {
+    text: 'Bad kerning bothers me.',
+    type: 'design',
+  },
+  {
+    text: 'I can style a satisfying-to-click button.',
+    type: 'front',
+  },
+  {
+    text: 'This bothers me: &lt;IMG SRC=\"\">.',
+    type: 'front',
+  },
+  {
+    text: 'I enjoy debugging CSS.',
+    type: 'front',
+  },
+  {
+    text: 'I know how to identify garbage collection using dev tools.',
+    type: 'js',
+  },
 ];
