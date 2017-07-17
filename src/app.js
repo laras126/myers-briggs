f2490ccbed1b50f6719f5103521ed81a3c81ef06
laras126 <laras126@gmail.com>
2017-07-17 16:40:46 -0700

    create element variables, move update bar math to inside click func

--- src/app.js
+++ src/app.js
@@ -5,72 +5,77 @@ import styles from './css/app.css';
 import * as ques from './questions.js';
 import * as Score from './scores.js';
 
-// Questions array import
-const questions = ques.questions;
+// Elements
+const QUESTION = document.querySelector('#js-question'),
+      QUESTION_TYPE = document.querySelector('#js-question-type'),
+      ANSWERS = document.querySelector('#js-answers');
 
-let Scores = Score.createScoresObject(questions),
+// Global variables
+var questions = ques.questions,
+    Scores = Score.createScoresObject(questions),
     currentQuestionIndex = 0,
     currentQuestionType,
     currentQuestionText;
-
-let isLastQuestion = () => {
-  if( currentQuestionIndex == questions.length - 1 ) {
-    return true;
-  } else {
-    return false;
-  }
-};
 
 function setCurrentQuestionText() {
   currentQuestionType = questions[currentQuestionIndex].type,
   currentQuestionText = questions[currentQuestionIndex].text;
 
-  document.querySelector('#js-question').innerHTML = currentQuestionText;
-  document.querySelector('#js-question-type').innerHTML = currentQuestionType;
+  QUESTION.innerHTML      = currentQuestionText;
+  QUESTION_TYPE.innerHTML = currentQuestionType;
 }
 
 function setEndOfQuizText() {
-  document.querySelector('#js-question').innerHTML = "No more questions.";
-  document.querySelector('.answers-list').innerHTML = "Done!";
+  QUESTION.innerHTML = "No more questions.";
+  ANSWERS.innerHTML = "Done!";
 }
 
-function updateBarWidth(el, val) {
-  let width = el.offsetWidth,
-      oldWidth = parseInt(width, 10),
-      newWidth = oldWidth + val*10 + 'px';
-  if( val !== 0 ) {
-    el.style.width = newWidth;
-  }
-}
-
-function updateQuestion() {
+function handleQuestionUpdate() {
   currentQuestionIndex++;
-  if (isLastQuestion()) {
+
+  let isLastQuestion = currentQuestionIndex === questions.length ? true : false;
+
+  if (isLastQuestion) {
     setEndOfQuizText();
   } else {
     setCurrentQuestionText();
   }
 }
 
-setCurrentQuestionText();
+function applyNewBarWidth(newWidth, el) {
+  el.style.width = newWidth + 'px';
+}
+
+// Set up quiz
+window.addEventListener('DOMContentLoaded', () => {
+  setCurrentQuestionText();
+});
 
 // Use event delegation to handle the click event
-document.getElementById('js-answers').addEventListener('click', function(e) {
-
-  updateQuestion();
+ANSWERS.addEventListener('click', (e) => {
 
   if(e.target && e.target.nodeName === 'LI') {
 
+    handleQuestionUpdate();
+
     let currentQuestion = questions[currentQuestionIndex - 1],
+        scoreMultiplier = currentQuestion.multiplier ? currentQuestion.multiplier : 1,
         targetType = currentQuestion.type,
         clickedScore = +e.target.dataset.score,
         targetEl = document.getElementById(targetType + 'Value');
 
+    let newBarWidth = ((el, val, multiplier) => {
+      let oldWidth = targetEl.offsetWidth,
+          widthToAdd = Math.round(val*multiplier),
+          newWidth = widthToAdd + oldWidth;
+      return newWidth;
+    })(targetEl, clickedScore, scoreMultiplier);
+
     // Update scores
     Scores[targetType] += clickedScore;
-    targetEl.innerHTML = Scores[targetScore];
+    targetEl.innerHTML = Scores[targetType];
 
-    updateBarWidth(targetEl, clickVal);
+    applyNewBarWidth(newBarWidth, targetEl);
 
   }
 
